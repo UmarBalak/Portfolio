56f79caae8884b2817eb41cdef246d2d0eec3723
Umar Balak <umarbalak35@gmail.com>
2024-09-21 21:48:01 +0530

    minor changes

--- streamlit-portfolio/app.py
+++ streamlit-portfolio/app.py
@@ -2,6 +2,8 @@ import streamlit as st
 from streamlit_timeline import timeline
 from streamlit_option_menu import option_menu
 import time
+import json
+import random
 
 ### METADATA
 st.set_page_config(
@@ -118,141 +120,6 @@ page = option_menu(
         orientation="horizontal",
     )
 
-# components.html(
-#     """
-# <!DOCTYPE html>
-# <html>
-# <head>
-# <meta name="viewport" content="width=device-width, initial-scale=1">
-# <style>
-# * {box-sizing: border-box;}
-# body {font-family: Verdana, sans-serif;}
-# .mySlides {display: none;}
-# img {vertical-align: middle;}
-
-# /* Slideshow container */
-# .slideshow-container {
-#   max-width: 1000px;
-#   position: relative;
-#   margin: auto;
-# }
-
-# /* Caption text */
-# .text {
-#   color: #f2f2f2;
-#   font-size: 15px;
-#   padding: 8px 12px;
-#   position: absolute;
-#   bottom: 8px;
-#   width: 100%;
-#   text-align: center;
-# }
-
-# /* Number text (1/3 etc) */
-# .numbertext {
-#   color: #f2f2f2;
-#   font-size: 12px;
-#   padding: 8px 12px;
-#   position: absolute;
-#   top: 0;
-# }
-
-# /* The dots/bullets/indicators */
-# .dot {
-#   height: 15px;
-#   width: 15px;
-#   margin: 0 2px;
-#   background-color: #bbb;
-#   border-radius: 50%;
-#   display: inline-block;
-#   transition: background-color 0.6s ease;
-# }
-
-# .active {
-#   background-color: #717171;
-# }
-
-# /* Fading animation */
-# .fade {
-#   animation-name: fade;
-#   animation-duration: 1.5s;
-# }
-
-# @keyframes fade {
-#   from {opacity: .4} 
-#   to {opacity: 1}
-# }
-
-# /* On smaller screens, decrease text size */
-# @media only screen and (max-width: 300px) {
-#   .text {font-size: 11px}
-# }
-# </style>
-# </head>
-# <body>
-
-# <h2>Automatic Slideshow</h2>
-# <p>Change image every 2 seconds:</p>
-
-# <div class="slideshow-container">
-
-# <div class="mySlides fade">
-#   <div class="numbertext">1 / 3</div>
-#   <img src="https://unsplash.com/photos/GJ8ZQV7eGmU/download?force=true&w=1920" style="width:100%">
-#   <div class="text">Caption Text</div>
-# </div>
-
-# <div class="mySlides fade">
-#   <div class="numbertext">2 / 3</div>
-#   <img src="https://unsplash.com/photos/eHlVZcSrjfg/download?force=true&w=1920" style="width:100%">
-#   <div class="text">Caption Two</div>
-# </div>
-
-# <div class="mySlides fade">
-#   <div class="numbertext">3 / 3</div>
-#   <img src="https://unsplash.com/photos/zVhYcSjd7-Q/download?force=true&w=1920" style="width:100%">
-#   <div class="text">Caption Three</div>
-# </div>
-
-# </div>
-# <br>
-
-# <div style="text-align:center">
-#   <span class="dot"></span> 
-#   <span class="dot"></span> 
-#   <span class="dot"></span> 
-# </div>
-
-# <script>
-# let slideIndex = 0;
-# showSlides();
-
-# function showSlides() {
-#   let i;
-#   let slides = document.getElementsByClassName("mySlides");
-#   let dots = document.getElementsByClassName("dot");
-#   for (i = 0; i < slides.length; i++) {
-#     slides[i].style.display = "none";  
-#   }
-#   slideIndex++;
-#   if (slideIndex > slides.length) {slideIndex = 1}    
-#   for (i = 0; i < dots.length; i++) {
-#     dots[i].className = dots[i].className.replace(" active", "");
-#   }
-#   slides[slideIndex-1].style.display = "block";  
-#   dots[slideIndex-1].className += " active";
-#   setTimeout(showSlides, 2000); // Change image every 2 seconds
-# }
-# </script>
-
-# </body>
-# </html> 
-
-#     """,
-#     height=600,
-# )
-
-
 ### SIDEBAR
 st.markdown(
     """
@@ -272,7 +139,7 @@ st.markdown(
     """,
     unsafe_allow_html=True
 )
-st.sidebar.markdown("<br><br>", unsafe_allow_html=True)
+# st.sidebar.markdown("<br>", unsafe_allow_html=True)
 st.sidebar.markdown(
     """
     <style>
@@ -309,7 +176,7 @@ gmail.markdown(
     """
     <div class="sidebar-button">
         <a href="mailto:umarbalak35@gmail.com" target="_blank" class="button">
-            <img src="https://lh3.googleusercontent.com/0rpHlrX8IG77awQMuUZpQ0zGWT7HRYtpncsuRnFo6V3c8Lh2hPjXnEuhDDd-OsLz1vua4ld2rlUYFAaBYk-rZCODmi2eJlwUEVsZgg" alt="Gmail" width="25" height="25">
+            <img src="https://lh3.googleusercontent.com/0rpHlrX8IG77awQMuUZpQ0zGWT7HRYtpncsuRnFo6V3c8Lh2hPjXnEuhDDd-OsLz1vua4ld2rlUYFAaBYk-rZCODmi2eJlwUEVsZgg" alt="Gmail" width="20" height="20">
         </a>
     </div>
     """, 
@@ -321,7 +188,7 @@ github.markdown(
     """
     <div class="sidebar-button">
         <a href="https://github.com/UmarBalak" target="_blank" class="button">
-            <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="GitHub" width="25" height="25">
+            <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="GitHub" width="20" height="20">
         </a>
     </div>
     """, 
@@ -333,7 +200,7 @@ linkedin.markdown(
     """
     <div class="sidebar-button">
         <a href="https://www.linkedin.com/in/umar-balak/" target="_blank" class="button">
-            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/LinkedIn_logo_initials.png/600px-LinkedIn_logo_initials.png" alt="LinkedIn" width="25" height="25">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/LinkedIn_logo_initials.png/600px-LinkedIn_logo_initials.png" alt="LinkedIn" width="20" height="20">
         </a>
     </div>
     """, 
@@ -342,19 +209,32 @@ linkedin.markdown(
 
 st.sidebar.write('---')
 
-st.sidebar.markdown(
-    """
-    <div style="text-align: center; font-size: 12px;">
-        A personal portfolio project © 2024.
-    </div>
-    """,
-    unsafe_allow_html=True
-)
+with open('quotes.json', 'r') as file:
+    quotes = json.load(file)
 
-### CONTENT
-# aboutme, proj, resume = st.tabs(["About me",
-#                                     "Projects", 
-#                                     "Resume"])
+random_quote = random.choice(quotes)
+# Display the random quote
+# st.sidebar.markdown(
+#     f'<p style="text-align: center; font-size: 12px;">“{random_quote["quote"]}” - <em>{random_quote["author"]}</em></p>',
+#     unsafe_allow_html=True
+# )
+# st.sidebar.markdown(
+#     f"""
+#     <div style="
+#         text-align: center; 
+#         font-size: 20px; 
+#         border: 1px solid #ccc; 
+#         border-radius: 8px; 
+#         padding: 5px; 
+#         margin-top: 20px;
+#         box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.1);
+#         ">
+#         “{random_quote["quote"]}” - <em>{random_quote["author"]}</em>
+#     </div>
+#     """,
+#     unsafe_allow_html=True
+# )
+
 if page == "About":
     timeline_data =                 {
                         "title": {
